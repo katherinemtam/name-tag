d5d395a2eb539f1415f7ca9894033a674bef8934
Katherine Tam <katherinemtam@gmail.com>
2020-12-03 15:56:25 -0800

    add buttons and functions to change background color

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ mySpecialButton.addEventListener('click', () => {
     mySpecialInput.value = '';
 })
 
+const changeColorPink = document.getElementById('change-color-pink')
+changeColorPink.addEventListener('click', () => {
+    document.body.style.backgroundColor = "pink";
+})
+
+const changeColorGreen = document.getElementById('change-color-green')
+changeColorGreen.addEventListener('click', () => {
+    document.body.style.backgroundColor = "lightgreen";
+})
+
+const changeColorBlue = document.getElementById('change-color-blue')
+changeColorBlue.addEventListener('click', () => {
+    document.body.style.backgroundColor = "lightblue";
+})
